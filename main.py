3f41df61759e40d7ac51181c88bdca34068346ea
Reet Awwsum <reetawwsum@yahoo.com>
2016-10-17 23:56:40 +0530

    Added char2id and id2char functions

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from __future__ import print_function
 
-from utils import *
 from model import *
 
 flags = tf.app.flags
 flags.DEFINE_integer('batch_size', 100, 'The size of training batch')
 flags.DEFINE_integer('num_unrollings', 10, 'Input sequence length')
+flags.DEFINE_integer('validation_size', 1000, 'Size of validation dataset')
 flags.DEFINE_string('dataset_dir', 'data', 'Directory name for the dataset')
 flags.DEFINE_string('dataset', 'text8.zip', 'Name of dataset')
-flags.DEFINE_integer('validation_size', 1000, 'Size of validation dataset')
 flags.DEFINE_string('batch_dataset_type', 'train_dataset', 'Dataset used for generating training batches')
 flags.DEFINE_string('validation_dataset_type', 'validation_dataset', 'Dataset used for validation')
 FLAGS = flags.FLAGS
--- utils.py
+++ utils.py
@@ -6,7 +6,24 @@ import zipfile
 import numpy as np
 import tensorflow as tf
 
-from ops import *
+def char2id(char):
+	first_letter = ord(string.ascii_lowercase[0])
+
+	if char in string.ascii_lowercase:
+		return ord(char) - first_letter + 1
+	elif char == ' ':
+		return 0
+	else:
+		print('Unexpected character: %s' % char)
+		return 0
+
+def id2char(charid):
+	first_letter = ord(string.ascii_lowercase[0])
+
+	if charid > 0:
+		return chr(charid + first_letter - 1)
+	else:
+		return ' '
 
 class Dataset:
 	'''Load dataset'''
